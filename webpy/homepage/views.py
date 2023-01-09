342425961a5cbaa9f1de4b89fc7b789fba4db888
Jason <jasonpavlas@icloud.com>
2023-01-08 23:25:44 -0500

    removed the homepage stuff

--- webpy/homepage/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
-from django.urls import reverse
-from django.views import generic
-
-class IndexView(generic.ListView):
-    template_name = 'homepage/index.html'
-
